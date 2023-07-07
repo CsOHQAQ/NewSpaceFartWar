5ef6f3d62f826bdcada20b982ca07f1e792f268a
CsOHQAQ <529353999@qq.com>
2023-07-07 11:10:11 +0800

    *导入玩家图片

---
+++ Assets/Scripts/GameScripts/PlayerController.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class PlayerController : MonoBehaviour
+{
+    Rigidbody2D rigidbody;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
