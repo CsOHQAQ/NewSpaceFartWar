2feedd9b3fef971a256c40058b10a8cc9b5dcf83
CsOHQAQ <529353999@qq.com>
2023-07-07 13:21:33 +0800

    *准备测试inputsystem

--- Assets/Scripts/GameScripts/PlayerController.cs
+++ Assets/Scripts/GameScripts/PlayerController.cs
@@ -1,18 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.InputSystem;
 using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     Rigidbody2D rigidbody;
+    Transform fartTransform;
+    Transform grabTransform;
+
     // Start is called before the first frame update
     void Start()
     {
         rigidbody = GetComponent<Rigidbody2D>();
+        fartTransform = transform.Find("fartTransform");
+        grabTransform = transform.Find("grabTransform");
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Move();
+    }
+    public void Move()
+    {
+
     }
 }
